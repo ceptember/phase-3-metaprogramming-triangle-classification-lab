02ad1239f97c5fc84ff0a87c9ab8d11d4e9a00f9
Christy Perozzi <christy.perozzi@gmail.com>
2022-02-09 10:32:35 -0500

    commit tree 4d4812e35b70b4b135e2a2796fc55c11392617a9

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,38 @@
 class Triangle
-  # write code here
+
+  attr_accessor :a, :b, :c
+
+  def initialize(a, b, c)
+    @a = a
+    @b = b
+    @c = c 
+
+  end
+
+  def kind
+    a = @a
+    b = @b
+    c = @c
+
+    if !(a > 0 && b > 0 && c > 0 && a+b > c && a+c > b &&  b + c > a)
+      raise TriangleError
+    elsif a == b && b == c
+      :equilateral
+    elsif a == b || a == c || b == c 
+      :isosceles
+    else
+      :scalene
+    end
+  end 
+
+  class TriangleError < StandardError
+    
+  end
+
 end
+
+
+
+test = Triangle.new(5, 3, 4)
+puts test.kind
+
